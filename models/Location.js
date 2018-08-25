dcf0043bd041bd7226d3185258f7198764a0bb4a
Abhishek Butola <abhishekbutola08@gmail.com>
2018-08-26 00:20:34 +0530

    added Location

--- models/Location.js
+++ models/Location.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//create geolocation schema
+const GeoSchema = new Schema({
+  type: {
+    default: 'Point',
+    type: String
+  },
+  coordinates: {
+    type: [Number],
+    index: '2dsphere'
+  }
+});
+
 const LocationSchema = new Schema({
   uid: {
     type: String
@@ -10,7 +22,8 @@ const LocationSchema = new Schema({
   },
   latitude: {
     type: Number
-  }
+  },
+  geometry: GeoSchema
 });
 
 module.exports = Location = mongoose.model('locations', LocationSchema);
--- routes/api/location.js
+++ routes/api/location.js
@@ -43,5 +43,53 @@ router.patch('/', (req, res) => {
     })
     .catch(err => console.log(err));
 });
+//@route POST api/location/near
+//@desc get nearest location userid
+//@access UNDEFINED
+router.post('/near', (req, res) => {
+  const longitude = req.body.long;
+  const latitude = req.body.lat;
+
+  Location.aggregate()
+    .near({
+      near: {
+        type: 'Point',
+        coordinates: [parseFloat(latitude), parseFloat(longitude)]
+      },
+      maxDistance: 100000,
+      spherical: true,
+      distanceField: 'dis'
+    })
+    .then(result => res.json(result))
+    .catch(err => console.log(err));
+});
+
+//@route PATCH api/location
+//@desc Update Location
+//@access UNDEFINED
+router.put('/test', (req, res) => {
+  const location = {
+    lat: req.body.lat,
+    lon: req.body.long
+  };
+  Location.findOne({ uid: req.body.uid })
+    .then(result => {
+      if (result) {
+        result.set({
+          geometry: {
+            coordinates: [parseFloat(location.lat), parseFloat(location.lon)]
+          }
+        });
+        console.log(result);
+        result
+          .save()
+          .then(res.json({ message: 'Success' }))
+          .catch(err => console.log(err));
+      } else {
+        return res.status(404).json({ message: 'User not found' });
+      }
+    })
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
